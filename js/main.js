a26f2f67c82e95401638ea48e89ab3ba8f3e4c00
GeoLift <contact.geolift@gmail.com>
2026-03-24 16:27:41 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -181,8 +181,10 @@ document.addEventListener("DOMContentLoaded", function() {
 window.addEventListener("scroll", function(){
 const btn = document.querySelector(".scroll-top");
 
+if(!btn) return;
+
 if(window.scrollY > 300){
-btn.style.display = "block";
+btn.style.display = "flex";
 }else{
 btn.style.display = "none";
 }
