829da5d9f3d72ac61dc7a4b3f01c9c4033e6feb4
GeoLift <contact.geolift@gmail.com>
2026-03-24 13:38:47 +0530

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,1 +1,10 @@
+function loadHTML(id, file){
+fetch(file)
+.then(res => res.text())
+.then(data => {
+document.getElementById(id).innerHTML = data;
+});
+}
 
+loadHTML("header", "/includes/header.html");
+loadHTML("footer", "/includes/footer.html");
